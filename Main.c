6a40144c3b5a06ffc92a8723de6343ed413d2c3e
yunes-mek <yunes.mekki@gmail.com>
2026-04-13 17:36:34 +0200

    Update Main.c

--- Main.c
+++ Main.c
@@ -1,5 +1,24 @@
 #include <stdlib.h>
 
 int main(){
+  int choix;
+
+  do{
+    printf("\n --- MENU PRINCIPAL--- \n");
+    printf("1. Mode Ensaignant \n");
+    printf("2. Mode Etudiant \n");
+    printf("3. Quitter \n");
+    scanf("%d", &choix);
+
+    if(choix == 1){
+      printf("Mode enseignant ...\n);
+    }
+    else if(choix == 2){
+        printf("Moee enseeignant ...\n);
+    }
+    else{
+        printf("Ce que vous avez entré n'est pas valide, veuillez entrer un nombre entre 1 et 3. \n");
+    }
+  } while(choix != 3);
 
 }
