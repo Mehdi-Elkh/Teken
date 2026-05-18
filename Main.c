2565be2b4c7c1496533ff30b1bec2018ae631edc
yunes-mek <yunes.mekki@gmail.com>
2026-05-18 21:26:08 +0200

    Update Main.c

--- Main.c
+++ Main.c
@@ -65,12 +65,12 @@ void construct_QCM(QCM *qcm){
  		printf("Veuillez enter une question");
 				
 		for(int i = 0 ; i < NB_CHOIX - 1 ; i++){
-				printf("Veuillez ecrire le choix %d : \n", i +1);
+				printf("Veuillez ecrire le choix %d : \n", i + 1);
             	fgets((*qcm).question[j].choix[i], TAILLE , stdin);
         
         	do{
                 
-            	printf("Est ce que le choix%d est t'il vrai ? \n", i);
+            	printf("Est ce que le choix%d est t'il vrai ? (1=oui , 0=non) \n", i+1);
         		scanf("%d", &(*qcm).question[j].vrai[i]);
         		getchar();
 				if((*qcm).question[j].vrai[i] != 1 && (*qcm).question[j].vrai[i] != 0){
