80327aa7d1d7a21a1db5f871598b27496123f692
yunes-mek <yunes.mekki@gmail.com>
2026-05-19 11:07:45 +0200

    Update bibliotheque.h

--- bibliotheque.h
+++ bibliotheque.h
@@ -1,1 +1,22 @@
-izdhizd
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+#define NB_CHOIX 4
+#define TAILLE 1000
+
+
+
+typedef struct {
+	int numero;
+	char intitule[TAILLE];
+    char choix[NB_CHOIX][TAILLE];
+    int vrai[NB_CHOIX];
+} Question;
+
+typedef struct {
+	char titre[100];
+    int nb_questions;
+    int negatif;
+    Question question[100];
+} QCM
