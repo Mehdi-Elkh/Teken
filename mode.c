149631962d34de9b213eab185be6d2f3200ce522
yunes-mek <yunes.mekki@gmail.com>
2026-05-19 17:15:52 +0200

    Update mode.c

--- mode.c
+++ mode.c
@@ -45,4 +45,49 @@ void mode_etudiant(){
 	else{
 	    printf("Choix invalide\n");
 	    return;
+	}	
+	for( int i=0; i < qcm.nb_questions;i++){
+		int juste =1;
+		printf("\nQuestion %d :\n",qcm.question[i].numero);
+		printf("%s", qcm.question[i].intitule);
+
+		for (int j=0;j< NB_CHOIX;j++){
+			printf("%d-",j+1);
+			printf("%s",qcm.question[i].choix[j]);
+		
+			printf(" (1 = vrai / 0 = faux / 2 = passer question) : ");
+    		resulta = scanf("%d", &rep[j]);
+            
+    		if (resulta != 1) {
+        		printf("Erreur\n");
+        		while (getchar() != '\n');
+        		rep[j] = 0;
+            }
+			if (rep[j] == 2) {
+        		printf("Question passée\n");
+        		juste = 0;
+       			break;
+			}
+
+			if (rep[j] != qcm.question[i].vrai[j]) {
+        		juste = 0;
+    		}
+    	}
+    	if (juste == 0) {
+    		if (rep[0] == 2) {
+                //On passe la question
+            } else {
+       		 	printf("Faux\n");
+        		if (qcm.negatif == 1) {
+                	note--;
+    			}
+			}
+		}
+		else {
+    		printf("Vrai\n");
+    		note++;
+		}  
 	}
+	 printf("Votre note est %d sur %d \n",note, qcm.nb_questions);
+
+}
