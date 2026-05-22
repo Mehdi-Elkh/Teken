37a591bea34890f3c216b34eb02abf9a0c2201f5
yunes-mek <yunes.mekki@gmail.com>
2026-05-22 19:37:44 +0200

    Update mode.c

--- mode.c
+++ mode.c
@@ -13,39 +13,25 @@ void mode_etudiant(){
     int note = 0 , rep[NB_CHOIX];
   
 	int resulta;
-	int choixQCM;              
+	int choixQCM;    
+	char titre[100];          
 	char fichier[100];
 	
-    printf("mode étudiant\n");
+ printf("mode étudiant\n");
 	afficher_QCM_dispo();
 	
 	printf("Choisissez le QCM a passer : ");
-	printf("Choisissez le QCM a passer : ");
-	resulta = scanf("%d", &choixQCM);
+	scanf("%d" , &choixQCM);
 	
-	if(resulta != 1){
-	    printf("Choix invalide\n");
-	    while(getchar() != '\n'){
-	    }
-	    return;
-	} 
-	if(choixQCM == 1){
-    	lire_QCM(&qcm, "maths.txt");
-	}
+	FILE *f = fopen("liste_qcm.txt","r");
+	
+	
 
-	else if(choixQCM == 2){
-	    lire_QCM(&qcm, "info.txt");
-	}
-	
-	else if(choixQCM == 3){
-	    lire_QCM(&qcm, "foot.txt");
-	}
-	
-	
-	else{
-	    printf("Choix invalide\n");
-	    return;
-	}	
+
+
+
+
+
 	for( int i=0; i < qcm.nb_questions;i++){
 		int juste =1;
 		printf("\nQuestion %d :\n",qcm.question[i].numero);
